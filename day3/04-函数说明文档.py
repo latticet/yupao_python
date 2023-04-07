ae13447ce59a54f37b3ae85094672378d4594427
zhangyaowen <zhangyaowenjy@163.com>
2023-04-07 17:13:56 +0800

    函数说明文档

---
+++ day3/04-函数说明文档.py
@@ -0,0 +1,29 @@
+# 函数说明文档
+# 说明：函数标准注释的写法
+
+"""
+语法：
+def fn():
+    '''
+    函数说明文档
+    '''
+    函数体
+"""
+
+
+def add(a, b):
+    """
+    求任意俩个数的和
+    :param a: 数字1 int
+    :param b: 数字2 int
+    :return: 2个数的和 int
+    """
+    pass
+
+
+# help(函数)
+# 查看函数的说明文档
+
+help(add)
+
+help(print)
