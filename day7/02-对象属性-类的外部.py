1d4d18036bf992da67910563cfa37d480139edbb
zhangyaowen <zhangyaowenjy@163.com>
2023-05-20 08:54:08 +0800

    对象方法

--- day7/02-对象属性-类的外部.py
+++ day7/02-对象属性-类的外部.py
@@ -5,4 +5,27 @@ class Person:
     # 类的内部
     pass
 
+
+# 创建对象
+p1 = Person()
+
 # 类的外部
+# TODO 设置属性
+# 语法：对象.属性名 = 属性值   obj.attr = value
+p1.name = 'hello'
+p1.age = 18
+
+# TODO 查看属性
+# 语法：obj.attr
+print(p1.name)
+print(p1.age)
+
+# TODO 修改属性
+# 语法：obj.attr = value
+p1.name = 'good'
+print(p1.name)
+
+# TODO 删除属性
+# del obj.attr
+del p1.name
+print(p1.name)
